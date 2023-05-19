779bc6fb30e2829ac0687468aa905f0ef7956f12
Zach Schillaci <40636930+zachschillaci27@users.noreply.github.com>
2023-05-19 22:17:16 +0200

    feat: Add workout records (w/ refactoring)

--- ingester/app.py
+++ ingester/app.py
@@ -3,26 +3,22 @@ Ingester module that converts Apple Health export zip file
 into influx db datapoints
 """
 import os
-import re
 import time
 import xml.etree.ElementTree as etree
 from shutil import unpack_archive
 from typing import Any, Union
 
+import dateutil.parser
 import gpxpy
-from dateutil.parser import parse
 from gpxpy.gpx import GPXTrackPoint
 from influxdb import InfluxDBClient
 
-CLIENT = InfluxDBClient("influx", 8086, database="health")
-PREFIX_RE = re.compile("HK.*Identifier(.+)$")
 ZIP_PATH = "/export.zip"
-UNZIP_PATH = "/export"
 ROUTES_PATH = "/export/apple_health_export/workout-routes/"
 EXPORT_PATH = "/export/apple_health_export/export.xml"
 
 
-def try_to_float(v: Any) -> Union[float, int]:
+def parse_float_with_try(v: Any) -> Union[float, int]:
     """convert v to float or 0"""
     try:
         return float(v)
@@ -33,10 +29,15 @@ def try_to_float(v: Any) -> Union[float, int]:
             return 0
 
 
-def format_route_point(name, point: GPXTrackPoint, next_point=None) -> dict[str, Any]:
+def parse_date_as_timestamp(v: Any) -> int:
+    return int(dateutil.parser.parse(v).timestamp())
+
+
+def format_route_point(
+    name: str, point: GPXTrackPoint, next_point=None
+) -> dict[str, Any]:
     """for a given `point`, creates an influxdb point
-    and computes speed and distance if `next_point` exists
-    """
+    and computes speed and distance if `next_point` exists"""
     slug_name = name.replace(" ", "-").replace(":", "-").lower()
     datapoint = {
         "measurement": "workout-routes",
@@ -58,22 +59,44 @@ def format_route_point(name, point: GPXTrackPoint, next_point=None) -> dict[str,
 
 def format_record(record: dict[str, Any]) -> dict[str, Any]:
     """format a export health xml record for influx"""
-    m = re.match(PREFIX_RE, record.get("type"))
-    measurement = m.group(1) if m else record.get("type")
-    value = try_to_float(record.get("value", 1))
+    measurement = (
+        record.get("type", "Record")
+        .removeprefix("HKQuantityTypeIdentifier")
+        .removeprefix("HKCategoryTypeIdentifier")
+        .removeprefix("HKDataType")
+    )
+    date = parse_date_as_timestamp(record.get("startDate", 0))
+    value = parse_float_with_try(record.get("value", 1))
     unit = record.get("unit", "unit")
-    date = int(parse(record.get("startDate")).timestamp())
     device = record.get("sourceName", "unknown")
 
     return {
         "measurement": measurement,
-        "tags": {"unit": unit, "device": device},
         "time": date,
         "fields": {"value": value},
+        "tags": {"unit": unit, "device": device},
     }
 
 
-def parse_workout_route(route_xml_file: str) -> None:
+def format_workout(record: dict[str, Any]) -> dict[str, Any]:
+    """format a export health xml workout record for influx"""
+    measurement = record.get("workoutActivityType", "Workout").removeprefix(
+        "HKWorkoutActivityType"
+    )
+    date = parse_date_as_timestamp(record.get("startDate", 0))
+    value = parse_float_with_try(record.get("duration", 0))
+    unit = record.get("durationUnit", "unit")
+    device = record.get("sourceName", "unknown")
+
+    return {
+        "measurement": measurement,
+        "time": date,
+        "fields": {"value": value},
+        "tags": {"unit": unit, "device": device},
+    }
+
+
+def parse_workout_route(client: InfluxDBClient, route_xml_file: str) -> None:
     with open(route_xml_file, "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
         for track in gpx.tracks:
@@ -89,65 +112,70 @@ def parse_workout_route(route_xml_file: str) -> None:
                             segment.points[i + 1] if i + 1 < num_points else None,
                         )
                     )
-            CLIENT.write_points(track_points, time_precision="s")
+            client.write_points(track_points, time_precision="s")
 
 
-def process_workout_routes() -> None:
+def process_workout_routes(client: InfluxDBClient) -> None:
     if os.path.exists(ROUTES_PATH) and os.path.isdir(ROUTES_PATH):
         print("Loading workout routes ...")
         for file in os.listdir(ROUTES_PATH):
             if file.endswith(".gpx"):
                 route_file = os.path.join(ROUTES_PATH, file)
-                parse_workout_route(route_file)
+                parse_workout_route(client, route_file)
     else:
         print("No workout routes found, skipping ...")
 
 
-def process_health_data() -> None:
+def process_health_data(client: InfluxDBClient) -> None:
     if not os.path.exists(EXPORT_PATH):
         print("No export.xml file found, skipping ...")
         return
-    formatted_records = []
+    records = []
     total_count = 0
     for _, elem in etree.iterparse(EXPORT_PATH):
         if elem.tag == "Record":
-            f = format_record(elem)
-            formatted_records.append(f)
+            records.append(format_record(elem))
+            elem.clear()
+        elif elem.tag == "Workout":
+            records.append(format_workout(elem))
             elem.clear()
 
-            # batch push every 10000
-            if len(formatted_records) == 10000:
-                total_count += 10000
-                CLIENT.write_points(formatted_records, time_precision="s")
-                del formatted_records
-                formatted_records = []
-                print("Inserted", total_count, "records")
+        # batch push every 10000
+        if len(records) == 10000:
+            total_count += 10000
+            client.write_points(records, time_precision="s")
+
+            del records
+            records = []
+            print("Inserted", total_count, "records")
 
     # push the rest
-    CLIENT.write_points(formatted_records, time_precision="s")
-    print("Total number of records:", total_count + len(formatted_records))
+    client.write_points(records, time_precision="s")
+    print("Total number of records:", total_count + len(records))
 
 
 if __name__ == "__main__":
     print("Unzipping the export file ...")
     try:
-        unpack_archive(ZIP_PATH, UNZIP_PATH)
+        unpack_archive(ZIP_PATH, "/export")
     except Exception as unzip_err:
         print("Unable to open export zip:", unzip_err)
         exit(1)
     print("Export file unzipped!")
 
+    client = InfluxDBClient("influx", 8086, database="health")
+
     while True:
         try:
-            CLIENT.ping()
-            CLIENT.drop_database("health")
-            CLIENT.create_database("health")
+            client.ping()
+            client.drop_database("health")
+            client.create_database("health")
             print("Influx is ready.")
             break
         except Exception:
             print("Waiting on influx to be ready..")
             time.sleep(1)
 
-    process_workout_routes()
-    process_health_data()
+    process_workout_routes(client)
+    process_health_data(client)
     print("All done! You can now check grafana.")
