853ad79ee2f3d800a027869e4266419b5bddf84c
k0rventen <corentin@farque.net>
2024-01-26 16:28:14 +0100

    feat: add provisioning test to grafana

--- tests/run.sh
+++ tests/run.sh
@@ -3,8 +3,10 @@ set -e
 
 chmod a+x tests.sh
 docker compose down
-docker compose up -d influx
 docker compose build
+
+# ingester/influx tests
+docker compose up -d influx
 docker compose exec influx apt update
 docker compose exec influx apt install jq -y
 docker compose cp tests.sh influx:/usr/local/bin/
@@ -15,5 +17,11 @@ do
   docker compose up --exit-code-from $service $service 
   docker compose exec influx tests.sh
 done
+
+# grafana test
+docker compose up -d grafana
+sleep 10
+docker compose exec grafana curl http://127.0.0.1:3000/api/dashboards/home -f -u 'admin:health' 
+
 
 docker compose down
