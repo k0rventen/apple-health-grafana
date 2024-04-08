f52af7c7a1a28f8c531b885973497b1b204cbd70
k0rventen <49311792+k0rventen@users.noreply.github.com>
2024-04-08 18:30:39 +0200

    feat: tests for CN exports

--- tests/run.sh
+++ tests/run.sh
@@ -10,7 +10,7 @@ docker compose exec influx apt install jq -y
 docker compose cp tests.sh influx:/usr/local/bin/
 
 # for each compose service, run it, check for output, then reset influx
-for service in test-ingester-std test-ingester-no-routes-uppercase test-ingester-malformed-xml test-ingester-malformed-xml-2
+for service in test-ingester-std test-ingester-no-routes-uppercase test-ingester-malformed-xml test-ingester-malformed-xml-2 test-ingester-cn
 do
   docker compose up --exit-code-from $service $service 
   docker compose exec influx tests.sh
